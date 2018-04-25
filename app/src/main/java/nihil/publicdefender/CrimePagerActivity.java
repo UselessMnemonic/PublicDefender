1f50d74a2d9273c5f0401259fcfe13561e8161be
fccstudent <fresnocitystudent@gmail.com>
2018-04-24 21:03:48 -0700

    GET THE LOCATION

--- app/src/main/java/nihil/publicdefender/CrimePagerActivity.java
+++ app/src/main/java/nihil/publicdefender/CrimePagerActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,7 +20,7 @@ import java.util.UUID;
  * Created by be127osx on 4/17/18.
  */
 
-public class CrimePagerActivity extends FragmentActivity {
+public class CrimePagerActivity extends AppCompatActivity {
 
     private static final String EXTRA_CRIME_ID = "nihil.publicdefender.extra_cime_id";
 
